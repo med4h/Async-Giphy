9cfb62908519101e4a38a5ad393d9a67a02ab5af
med4h <adrian.meda6@gmail.com>
2024-11-05 20:19:09 -0800

    added code for async giphy project

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
 
+console.log(process.env.API_KEY);
+
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`;
+
+async function getImage(query) {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const image = data.data[Math.floor(Math.random() * 24)].images.original.url;
+    console.log(image);
+    return image;
+}
+
+getImage("dog");
+
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
